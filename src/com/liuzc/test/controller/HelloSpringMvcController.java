cd9b6ecce6086ea01d8c4adccd13a11013a8d344
liuzc <15827717049@163.com>
2021-06-06 11:18:03 +0800

    Initial commit

---
+++ src/com/liuzc/test/controller/HelloSpringMvcController.java
@@ -0,0 +1,19 @@
+package com.liuzc.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author 15827
+ */
+@Controller
+public class HelloSpringMvcController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return  "hello";
+    }
+}
---
+++ web/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 15827
+  Date: 2021/6/6
+  Time: 9:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello world</title>
+</head>
+<body>
+     245645456456456
+</body>
+</html>
